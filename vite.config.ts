2d14a55e98e584f8b7193b020ebe1acc1735b591
zulalhafizh <ahmadzulfikaralhafizh@gmail.com>
2026-07-13 12:02:39 +0700

    install plugin dev-tools untuk mempermudah development

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,15 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'node:path'
+import VueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), tailwindcss()],
+  plugins: [
+    vue(),
+    VueDevTools(),
+    tailwindcss()
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
